d1fcc94c42a07c7717c3b45771c1d244c5edb13b
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2025-05-26 00:40:50 +0100

    remove the last option

--- components/wallet-provider.tsx
+++ components/wallet-provider.tsx
@@ -7,7 +7,6 @@ import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom"
 import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare"
 import { GlowWalletAdapter } from "@solana/wallet-adapter-glow"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
-import  WalletConnectConnector  from '@walletconnect/web3-provider';
 
 // Import wallet adapter CSS
 require("@solana/wallet-adapter-react-ui/styles.css")
@@ -16,14 +15,9 @@ export function WalletContextProvider({ children }: { children: React.ReactNode 
   // Use a public RPC endpoint that doesn't require authentication
   const endpoint = useMemo(() =>  'https://api.mainnet-beta.solana.com', [])
   
-const WALLETCONNECT_RPC = 'https://api.mainnet-beta.solana.com';
+  
 
-    const walletConnectConnector = new WalletConnectConnector({
-    rpc: { 101: WALLETCONNECT_RPC },
-    qrcode: true,
-  });
-
-  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new GlowWalletAdapter(), walletConnectConnector], [])
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new GlowWalletAdapter()], [])
 
   return (
     <ConnectionProvider endpoint={endpoint}>
